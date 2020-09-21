eda25358934f4b995dce4d722c26b268ca28a8d8
chocownd <april174@gmail.com>
2020-09-22 05:05:01 +0900

    bugfix about ui refresh

--- src/installer_ui.py
+++ src/installer_ui.py
@@ -1,4 +1,8 @@
-from typing import Dict
+from typing import Dict, Set, List
+
+from src.install.InstallServiceDiscovery import InstallServiceDiscovery
+from src.install.PkgCatalog import PkgCatalog
+from src.install.SuggestInstallCandidatePkgService import SuggestInstallCandidatePkgService
 from src.install.installer.PkgInstaller import PkgInstaller
 from src.install.installer.ExitInstaller import ExitInstaller
 
@@ -7,17 +11,10 @@ class InstallerUI:
     __exit_cmd_code = -1
     __exist_installer = ExitInstaller()
 
-    def __init__(self, cmd_installer_map: Dict[int, PkgInstaller]):
-        self.__cmd_installer_map = cmd_installer_map
+    def __init__(self, install_service_discovery: InstallServiceDiscovery):
+        self.__install_service_discovery = install_service_discovery
+        self.__install_dict: Dict[int, PkgInstaller] = self.__refresh_install_dict()
         super().__init__()
-
-    @staticmethod
-    def __input_cmd() -> int:
-        cmd_str = input()
-        try:
-            return int(cmd_str)
-        except ValueError:
-            raise TypeError(f"cmd should be number but not, '{cmd_str}'")
 
     def draw(self):
         while True:
@@ -32,12 +29,36 @@ class InstallerUI:
                 continue
             self.__run_command(cmd)
 
+    def __refresh_install_dict(self) -> Dict[int, PkgInstaller]:
+        install_candidates: Set[PkgCatalog] = SuggestInstallCandidatePkgService.suggest(
+            self.__install_service_discovery.pkg_dependency_dag_provider(),
+            self.__install_service_discovery.pkg_install_check_service()
+        )
+        installer_list: List[PkgInstaller] = sorted(
+            list(
+                map(
+                    lambda it: self.__install_service_discovery.installer_resolver().execute(it),
+                    install_candidates)),
+            key=lambda it: it.one_line_description())
+        installer_dict: Dict[int, PkgInstaller] = {}
+        for idx in range(0, len(installer_list)):
+            installer_dict[idx + 1] = installer_list[idx]
+        return installer_dict
+
+    @staticmethod
+    def __input_cmd() -> int:
+        cmd_str = input()
+        try:
+            return int(cmd_str)
+        except ValueError:
+            raise TypeError(f"cmd should be number but not, '{cmd_str}'")
+
     def __print_ui(self):
         print('********        xnixc installer        ********\n')
-        for cmd in sorted(list(filter(lambda it: it > 0, self.__cmd_installer_map.keys()))):
+        for cmd in sorted(list(filter(lambda it: it > 0, self.__install_dict.keys()))):
             print('[{:-3} ] {}'.format(
                 cmd,
-                self.__cmd_installer_map[cmd].one_line_description()))
+                self.__install_dict[cmd].one_line_description()))
 
         print('[{:-3} ] {}'.format(
             self.__exit_cmd_code,
@@ -48,12 +69,12 @@ class InstallerUI:
     def __check_cmd_valid(self, cmd):
         if cmd == self.__exit_cmd_code:
             return
-        if cmd not in self.__cmd_installer_map.keys():
+        if cmd not in self.__install_dict.keys():
             raise KeyError(f"command {cmd} is not exist in list")
 
     def __run_command(self, cmd: int):
         if cmd == self.__exit_cmd_code:
             self.__exist_installer.install()
         else:
-            installer = self.__cmd_installer_map[cmd]
+            installer = self.__install_dict[cmd]
             installer.install()
--- src/main.py
+++ src/main.py
@@ -1,28 +1,7 @@
-from typing import Set, List, Dict
-
 from src.install.InstallServiceDiscovery import InstallServiceDiscovery
-from src.install.PkgCatalog import PkgCatalog
-from src.install.SuggestInstallCandidatePkgService import SuggestInstallCandidatePkgService
-from src.install.installer.PkgInstaller import PkgInstaller
 from src.installer_ui import InstallerUI
 from src.utils.os.OSResolver import OSResolver
 
 if __name__ == '__main__':
-    install_service_discovery = InstallServiceDiscovery(OSResolver.resolve())
-
-    install_candidates: Set[PkgCatalog] = SuggestInstallCandidatePkgService.suggest(
-        install_service_discovery.pkg_dependency_dag_provider(),
-        install_service_discovery.pkg_install_check_service()
-    )
-    installer_list: List[PkgInstaller] = sorted(
-        list(
-            map(
-                lambda it: install_service_discovery.installer_resolver().execute(it),
-                install_candidates)),
-        key=lambda it: it.one_line_description())
-    installer_dict: Dict[int, PkgInstaller] = {}
-    for idx in range(0, len(installer_list)):
-        installer_dict[idx + 1] = installer_list[idx]
-
-    ui = InstallerUI(installer_dict)
-    InstallerUI.draw(ui)
+    ui = InstallerUI(InstallServiceDiscovery(OSResolver.resolve()))
+    ui.draw()
