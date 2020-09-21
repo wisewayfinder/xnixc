2d3d8bf947d94afeacfdd7a344ade0d273214e5f
chocownd <april174@gmail.com>
2020-09-22 04:22:52 +0900

    update ls command to hide err msg

--- src/install/check/Ubuntu2004PkgInstallCheckService.py
+++ src/install/check/Ubuntu2004PkgInstallCheckService.py
@@ -14,12 +14,12 @@ class Ubuntu2004PkgInstallCheckService(PkgInstallCheckService):
             PkgCatalog.GIT: lambda _: subprocess.run(['which', 'git'], capture_output=True).returncode == 0,
             PkgCatalog.CTAGS: lambda _: subprocess.run(['which', 'ctags'], capture_output=True).returncode == 0,
             PkgCatalog.CMAKE: lambda _: subprocess.run(['which', 'cmake'], capture_output=True).returncode == 0,
-            PkgCatalog.VUNDLE: lambda _: os.system('ls ~/.vim/bundle | grep Vundle.vim > /dev/null') == 0,
-            PkgCatalog.YCM: lambda _: os.system('ls ~/.vim/bundle | grep YouCompleteMe > /dev/null') == 0,
-            PkgCatalog.NERD_TREE: lambda _: os.system('ls ~/.vim/bundle | grep nerdtree > /dev/null') == 0,
+            PkgCatalog.VUNDLE: lambda _: os.system('(ls ~/.vim/bundle | grep Vundle.vim) >/dev/null 2>&1') == 0,
+            PkgCatalog.YCM: lambda _: os.system('(ls ~/.vim/bundle | grep YouCompleteMe) >/dev/null 2>&1') == 0,
+            PkgCatalog.NERD_TREE: lambda _: os.system('(ls ~/.vim/bundle | grep nerdtree) >/dev/null 2>&1') == 0,
             PkgCatalog.CSCOPE: lambda _: subprocess.run(['which', 'cscope'], capture_output=True).returncode == 0,
-            PkgCatalog.TAG_LIST: lambda _: os.system('ls ~/.vim/bundle | grep taglist.vim > /dev/null') == 0,
-            PkgCatalog.EASY_TAG: lambda _: os.system('ls ~/.vim/bundle | grep vim-easytags > /dev/null') == 0,
-            PkgCatalog.VIM_AIRLINE: lambda _: os.system('ls ~/.vim/bundle | grep vim-airline > /dev/null') == 0,
-            PkgCatalog.XNIXC: lambda _: os.system('ls ~/.xnixc | grep .xnixc > /dev/null') == 0
+            PkgCatalog.TAG_LIST: lambda _: os.system('(ls ~/.vim/bundle | grep taglist.vim) >/dev/null 2>&1') == 0,
+            PkgCatalog.EASY_TAG: lambda _: os.system('(ls ~/.vim/bundle | grep vim-easytags) >/dev/null 2>&1') == 0,
+            PkgCatalog.VIM_AIRLINE: lambda _: os.system('(ls ~/.vim/bundle | grep vim-airline) >/dev/null 2>&1') == 0,
+            PkgCatalog.XNIXC: lambda _: os.system('(ls ~/.xnixc | grep .xnixc) >/dev/null 2>&1') == 0
         }
