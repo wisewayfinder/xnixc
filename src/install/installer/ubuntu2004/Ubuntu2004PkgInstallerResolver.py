5c35608acafd994615c1d5a24a5cf92850aaa085
chocownd <april174@gmail.com>
2020-09-23 23:34:41 +0900

    install cmake with pw

--- src/install/installer/ubuntu2004/Ubuntu2004PkgInstallerResolver.py
+++ src/install/installer/ubuntu2004/Ubuntu2004PkgInstallerResolver.py
@@ -30,7 +30,7 @@ class Ubuntu2004PkgInstallerResolver(PkgInstallerResolver):
             PkgCatalog.VIM: lambda _: Ubuntu2004VimInstaller(),
             PkgCatalog.GIT: lambda _: Ubuntu2004GitInstaller(),
             PkgCatalog.CTAGS: lambda _: Ubuntu2004CtagsInstaller(),
-            PkgCatalog.CMAKE: lambda _: Ubuntu2004CMakeInstaller(),
+            PkgCatalog.CMAKE: lambda _: self.__installer_with_pw(Ubuntu2004CMakeInstaller),
             PkgCatalog.VUNDLE: lambda _: Ubuntu2004VundleInstaller(),
             PkgCatalog.YCM: lambda _: Ubuntu2004YcmInstaller(),
             PkgCatalog.NERD_TREE: lambda _: Ubuntu2004NerdTreeInstaller(),
