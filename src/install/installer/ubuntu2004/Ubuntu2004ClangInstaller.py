9ffbcf1427d7b4b6ab62c4740ee1aa100ad85cdd
chocownd <april174@gmail.com>
2020-09-23 22:40:58 +0900

    update clang install with sudo feature

--- src/install/installer/ubuntu2004/Ubuntu2004ClangInstaller.py
+++ src/install/installer/ubuntu2004/Ubuntu2004ClangInstaller.py
@@ -4,15 +4,26 @@ from src.install.installer.PkgInstaller import PkgInstaller
 
 
 # https://apt.llvm.org/
+from src.utils.os.SudoRunner import SudoRunner
+
+
 class Ubuntu2004ClangInstaller(PkgInstaller):
+    def __init__(self, sudo_password: str = None) -> None:
+        self.__sudo_pw = sudo_password
+        super().__init__()
+
     def one_line_description(self) -> str:
         return 'Clang'
 
-    def install(self) -> None:
+    def install(self):
         os.system(f"mkdir {Ubuntu2004ClangInstaller.__tmp_path()}")
         os.system(f"cd {Ubuntu2004ClangInstaller.__tmp_path()} && wget https://apt.llvm.org/llvm.sh")
         os.system(f"cd {Ubuntu2004ClangInstaller.__tmp_path()} && chmod +x llvm.sh")
-        os.system(f"cd {Ubuntu2004ClangInstaller.__tmp_path()} && sudo ./llvm.sh {self.clang_version()}")
+        llvm_install_cmd = f"cd {Ubuntu2004ClangInstaller.__tmp_path()} && sudo ./llvm.sh {Ubuntu2004ClangInstaller.clang_version()}"
+        if self.__sudo_pw is None:
+            os.system(llvm_install_cmd)
+        else:
+            SudoRunner.run(llvm_install_cmd, self.__sudo_pw)
         os.system(f"rm -rf {Ubuntu2004ClangInstaller.__tmp_path()}")
 
     @staticmethod
